40dd9e10af6122dbb798ad8e3f793c7e7c6e7242
GUNTUKU NARESH <guntuku001@gmail.com>
2023-10-15 23:17:58 +0530

    Added background images for buttons

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ for(var i=0;i<document.querySelectorAll(".drum").length;i++){
     document.querySelectorAll("button")[i].addEventListener("click",handleClick);
 }
 function handleClick(){
-    alert("Heyy Clicked!")
+    var audio=new Audio('sounds/tom-1.mp3');
+    audio.play();
 }
